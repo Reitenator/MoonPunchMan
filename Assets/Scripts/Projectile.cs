d71f6465b93433fb11286321c51ecaed876d7892
reitenator <tore.reiten@gmail.com>
2020-11-23 23:55:40 +0100

    new update

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Projectile : MonoBehaviour {
+
+    MeshRenderer mesh;
+    float displacementAmount;
+    Animator anim;
+
+    private void Start() {
+        mesh = GetComponent<MeshRenderer>();
+        anim = GetComponent<Animator>();    
+    }
+    private void Awake() {
+        Spawn();
+    }
+
+
+    private void Spawn() {
+        displacementAmount = -1;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        displacementAmount = Mathf.Lerp(displacementAmount, 0.1f, Time.deltaTime);
+        mesh.material.SetFloat("_Amount", displacementAmount);
+
         
     }
 }
